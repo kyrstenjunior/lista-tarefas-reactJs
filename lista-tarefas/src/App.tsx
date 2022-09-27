5a96cbe6e42089b888e73932bfff928abba49542
kyrsten-note <kyrstenjr@gmail.com>
2022-09-27 18:00:46 -0300

    criação inicial da aplicação de tarefas

---
+++ lista-tarefas/src/App.tsx
@@ -0,0 +1,14 @@
+import { Text } from 'react';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Text>
+        Lista de Tarefas
+      </Text>
+    </div>
+  )
+}
+
+export default App
